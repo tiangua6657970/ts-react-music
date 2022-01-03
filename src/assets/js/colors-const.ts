4a3fc23e1672e3db1feca399d89b2b2a13b7ae87
甜瓜 <tiangua_6657970@163.com>
2022-01-03 22:59:45 +0800

    init

---
+++ src/assets/js/colors-const.ts
@@ -0,0 +1,20 @@
+// 颜色定义规范
+export const color_background = '#222'
+export const color_background_d = 'rgba(0, 0, 0, 0.3)'
+export const color_highlight_background = '#333'
+export const color_dialog_background = '#666'
+export const color_theme = '#ffcd32'
+export const color_theme_d = 'rgba(255, 205, 49, 0.5)'
+export const color_sub_theme = '#d93f30'
+export const color_text = '#ffffff';
+export const color_text_d = 'rgba(255, 255, 255, 0.3)'
+export const color_text_l = 'rgba(255, 255, 255, 0.5)'
+export const color_text_ll = 'rgba(255, 255, 255, 0.8)'
+
+// 字体定义规范
+export const font_size_small_s = '10px'
+export const font_size_small = '12px'
+export const font_size_medium = '14px'
+export const font_size_medium_x = '16px'
+export const font_size_large = '18px';
+export const font_size_large_x = '22px'
